7124e630934b9850f8e5f78dd2a3eacb8543ed54
Roy Magen <magenroy@gmail.com>
2014-06-08 23:10:52 +0000

    lots of changes, mostly to IO

---
+++ FibIO.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import System.Environment
+import Control.Monad
+import Fibonacci
+
+usage :: IO ()
+usage = do
+	putStrLn "one input for the nth fibonacci term,"
+	putStrLn "3 inputs for the nth fibonacci term with a and b as edge cases,"
+	putStrLn "(add)|(mult)|(div)|(log)|(exp(i|f)|(concat) and 3 inputs for the nth fibonacci term with a and b as edge cases for function f"
+
+
+-- should make option to list terms
+-- this is obviously not ideal at the moment (so much copy and paste)
+fibIO :: [String] -> IO ()
+fibIO ["help"] = usage
+fibIO [n] = print (fib !! (read n))
+fibIO [n,a,b] = print (gfib (+) ((read a) :: Integer) ((read b) :: Integer) !! ((read n) :: Int))
+fibIO ["add",n,a,b] = print (gfib (+) ((read a) :: Integer) ((read b) :: Integer) !! ((read n) :: Int))
+fibIO ["mult",n,a,b] = print (gfib (*) ((read a) :: Integer) ((read b) :: Integer) !! ((read n) :: Int))
+fibIO ["div",n,a,b] = print (gfib (/) ((read a) :: Double) ((read b) :: Double) !! ((read n) :: Int))
+fibIO ["log",n,a,b] = print (gfib logBase ((read a) :: Double) ((read b) :: Double) !! ((read n) :: Int))
+fibIO ["expi",n,a,b] = print (gfib (^) ((read a) :: Integer) ((read b) :: Integer) !! ((read n) :: Int))
+fibIO ["expf",n,a,b] = print (gfib (**) ((read a) :: Double) ((read b) :: Double) !! ((read n) :: Int))
+fibIO ["concat",n,a,b] = print (gfib (++) ((read a) :: String) ((read b) :: String) !! ((read n) :: Int))
+
+main :: IO ()
+main = do
+	args <- getArgs
+	fibIO args
--- Fibonacci.hs
+++ Fibonacci.hs
@@ -1,3 +1,5 @@
+module Fibonacci where
+
 -- gfib :: (Num n, Ord n) => [n] -> [n -> n -> n] -> Int -> n
 -- gfib edges funcs tn
 --     | tn <= (length edges) = edges !! (tn - 1)
@@ -9,9 +11,9 @@
 -- fib :: (Num n, Ord n) => Int -> n
 -- fib = let add :: (Num n) => n -> n -> n; add a b = a + b in gfib [0,1] [add]
 
-gFib :: [a -> a -> a] -> [a] -> [a]
-gFib fs (e:es) = let a = foldr ($) (last es) $ zipWith ($) fs (e:es)
-                 in e:gFib fs (es ++ [a])
+gfib0 :: [a -> a -> a] -> [a] -> [a]
+gfib0 fs (e:es) = let a = foldr ($) (last es) $ zipWith ($) fs (e:es)
+                 in e:gfib0 fs (es ++ [a])
 
 --fib :: Num a => Int -> a
 --fib n = gfib [(+)] [0,1] !! (n - 1)
@@ -19,5 +21,12 @@ gFib fs (e:es) = let a = foldr ($) (last es) $ zipWith ($) fs (e:es)
 simpleGFib :: (a -> a -> a) -> a -> a -> [a]
 simpleGFib f a b = a:simpleGFib f b (f a b)
 
+gfib1 :: ([a] -> [a]) -> [a] -> [a]
+gfib1 f es = let next = f es in next ++ (gfib1 f $ es ++ next)
+
+test base init samples = let diff = round $ logBase base init in map (\x -> round $ logBase base x) (take samples $ gfib1 (\x -> [sum x]) [init]) == [diff..(samples + diff -1)]
+
+gfib = simpleGFib
+
 fib :: Integral a => [a]
-fib = simpleGFib (+) 0 1
+fib = gfib (+) 0 1
